184e12fbbde6b5622faebc5eff0cea4f8d571c1f
Zak Fargo <zak@fargobose.com>
2018-09-04 11:57:30 -0700

    finishing tests

--- test/unit/PinkHair.Common.Test.Tests/TestExtensions.cs
+++ test/unit/PinkHair.Common.Test.Tests/TestExtensions.cs
@@ -3,14 +3,116 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PinkHair.Common.Test.Tests
 {
     using PinkHair.Common;
+    using System;
+    using System.Threading.Tasks;
 
     [TestClass]
     public class TestExtensions : ContextAwareTestClass
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Success_RunExceptionalCode()
         {
+            this.RunExpectingException<InvalidOperationException>(() =>
+            {
+                Console.WriteLine("foo");
 
+                Action foo = () =>
+                {
+                    throw new InvalidOperationException();
+                };
+
+                Action bar = () =>
+                {
+                    foo();
+                };
+
+                bar();
+            });
+        }
+
+        [TestMethod]
+        public void Test_NonSuccess_RunExceptionalCode()
+        {
+            try
+            {
+                this.RunExpectingException<DivideByZeroException>(() =>
+                {
+                    Console.WriteLine("foo");
+
+                    Action foo = () =>
+                    {
+                        throw new InvalidOperationException();
+                    };
+
+                    Action bar = () =>
+                    {
+                        foo();
+                    };
+
+                    bar();
+                });
+
+                Assert.Fail("Exception should break before here");
+            }
+            catch(InvalidOperationException)
+            {
+                // success
+            }
+        }
+
+        [TestMethod]
+        public async Task Test_Success_RunExceptionalCodeAsync()
+        {
+            await this.RunExpectingException<InvalidOperationException>(async () =>
+            {
+                await Task.Yield();
+
+                Console.WriteLine("foo");
+
+                Action foo = () =>
+                {
+                    throw new InvalidOperationException();
+                };
+
+                Action bar = () =>
+                {
+                    foo();
+                };
+
+                bar();
+            });
+        }
+
+        [TestMethod]
+        public async Task Test_NonSuccess_RunExceptionalCodeAsync()
+        {
+            try
+            {
+                await this.RunExpectingException<DivideByZeroException>(async () =>
+                {
+                    await Task.Yield();
+
+                    Console.WriteLine("foo");
+
+                    Action foo = () =>
+                    {
+                        throw new InvalidOperationException();
+                    };
+
+                    Action bar = () =>
+                    {
+                        foo();
+                    };
+
+                    bar();
+                });
+
+                Assert.Fail("Exception should break before here");
+            }
+            catch (InvalidOperationException)
+            {
+                // success
+            }
         }
     }
 }
